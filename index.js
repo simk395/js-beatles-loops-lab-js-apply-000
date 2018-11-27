99ad98044af5c9e596b409dace4ebe7e7e062e1a
Simon Kong <simon.kong95@gmail.com>
2018-11-27 00:26:43 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // add solution here
+function theBeatlesPlay(musicians, instruments){
+  var beatles = [];
+  for (var i = 0; i < musicians.length; i++) {
+    beatles[i] = musicians[i] + " plays " + instruments[i];
+    return beatles[i] +",";
+  }
+}
+
+function johnLennonFacts(facts){
+  var i = 0;
+  while(i < facts.length)
+  facts[i] = facts[i] + "!!!";
+  i++;
+  return facts[i-1];
+}
